140bdcdf933da90399613e15da7dfa25258e4e9c
Danyal4040 <57940037+Danyal4040@users.noreply.github.com>
2020-12-26 21:04:44 -0800

    Add files via upload

---
+++ Customers/Customer_Form-1/Customer_Form-1/Form1.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Ref = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.F_Name = New System.Windows.Forms.TextBox()
+        Me.L_Name = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.CNIC = New System.Windows.Forms.TextBox()
+        Me.D_t = New System.Windows.Forms.DateTimePicker()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Bill_Number = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Address = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.phone = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Gender = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Reff#"
+        '
+        'Ref
+        '
+        Me.Ref.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Ref.Location = New System.Drawing.Point(136, 60)
+        Me.Ref.Multiline = True
+        Me.Ref.Name = "Ref"
+        Me.Ref.Size = New System.Drawing.Size(250, 20)
+        Me.Ref.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(22, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(22, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 25)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Last Name"
+        '
+        'F_Name
+        '
+        Me.F_Name.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.F_Name.Location = New System.Drawing.Point(151, 107)
+        Me.F_Name.Name = "F_Name"
+        Me.F_Name.Size = New System.Drawing.Size(250, 21)
+        Me.F_Name.TabIndex = 4
+        '
+        'L_Name
+        '
+        Me.L_Name.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.L_Name.Location = New System.Drawing.Point(178, 134)
+        Me.L_Name.Name = "L_Name"
+        Me.L_Name.Size = New System.Drawing.Size(252, 21)
+        Me.L_Name.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(22, 177)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Gender"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(22, 212)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 25)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "CNIC No#"
+        '
+        'CNIC
+        '
+        Me.CNIC.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CNIC.Location = New System.Drawing.Point(136, 218)
+        Me.CNIC.Name = "CNIC"
+        Me.CNIC.Size = New System.Drawing.Size(250, 21)
+        Me.CNIC.TabIndex = 11
+        '
+        'D_t
+        '
+        Me.D_t.Location = New System.Drawing.Point(138, 300)
+        Me.D_t.Name = "D_t"
+        Me.D_t.Size = New System.Drawing.Size(248, 20)
+        Me.D_t.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(24, 297)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(104, 25)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Enter Date"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(22, 251)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(111, 25)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Bill Number"
+        '
+        'Bill_Number
+        '
+        Me.Bill_Number.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bill_Number.Location = New System.Drawing.Point(136, 257)
+        Me.Bill_Number.Name = "Bill_Number"
+        Me.Bill_Number.Size = New System.Drawing.Size(250, 21)
+        Me.Bill_Number.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(26, 342)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(85, 25)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Address"
+        '
+        'Address
+        '
+        Me.Address.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Address.Location = New System.Drawing.Point(151, 342)
+        Me.Address.Name = "Address"
+        Me.Address.Size = New System.Drawing.Size(250, 21)
+        Me.Address.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(26, 388)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(85, 25)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Phone #"
+        '
+        'phone
+        '
+        Me.phone.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.phone.Location = New System.Drawing.Point(165, 388)
+        Me.phone.Name = "phone"
+        Me.phone.Size = New System.Drawing.Size(191, 21)
+        Me.phone.TabIndex = 19
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(136, 437)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 30)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Add" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(378, 437)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(115, 30)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(257, 437)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 30)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Gender
+        '
+        Me.Gender.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Gender.Location = New System.Drawing.Point(136, 181)
+        Me.Gender.Multiline = True
+        Me.Gender.Name = "Gender"
+        Me.Gender.Size = New System.Drawing.Size(81, 21)
+        Me.Gender.TabIndex = 23
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(512, 489)
+        Me.Controls.Add(Me.Gender)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.phone)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Address)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Bill_Number)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.D_t)
+        Me.Controls.Add(Me.CNIC)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.L_Name)
+        Me.Controls.Add(Me.F_Name)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Ref)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Add Customer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Ref As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents F_Name As TextBox
+    Friend WithEvents L_Name As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents CNIC As TextBox
+    Friend WithEvents D_t As DateTimePicker
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Bill_Number As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Address As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents phone As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Gender As TextBox
+End Class
---
+++ Customers/Customer_Form-1/Customer_Form-1/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class Form1
+
+    Dim con As OleDbConnection
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles phone.TextChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        clear()
+    End Sub
+    Sub clear()
+        Ref.Clear()
+        F_Name.Clear()
+        L_Name.Clear()
+        Gender.Clear()
+        CNIC.Clear()
+        Bill_Number.Clear()
+        Address.Clear()
+        phone.Clear()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Dim Insertquery As String = "INSERT INTO Table1 (Refn_N, F_name, L_name, Gender,,CNIC_N) values(@Refn_N, @F_name , @L_name ,@gender,@CNIC_N)"
+
+        Runquery(Insertquery)
+
+
+
+
+    End Sub
+    Public Sub Runquery(ByVal query As String)
+        con = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Danyal\Desktop\Customers\Data_base\Database3.mdb")
+        Dim cmd As New OleDbCommand(query, con)
+        cmd.Parameters.AddWithValue("@Refn_N", Ref.Text)
+        cmd.Parameters.AddWithValue("@F_name", F_Name.Text)
+        cmd.Parameters.AddWithValue("@L_name", L_Name.Text)
+        cmd.Parameters.AddWithValue("@gender", Gender.Text)
+        cmd.Parameters.AddWithValue("@CNIC_N", CNIC.Text)
+        cmd.Parameters.AddWithValue("@Bill_N", Bill_Number.Text)
+        cmd.Parameters.AddWithValue("@date", D_t.Text)
+        cmd.Parameters.AddWithValue("@address", Address.Text)
+        cmd.Parameters.AddWithValue("@P_N", phone.Text)
+        con.Open()
+        cmd.ExecuteNonQuery()
+        con.Close()
+
+
+    End Sub
+End Class
